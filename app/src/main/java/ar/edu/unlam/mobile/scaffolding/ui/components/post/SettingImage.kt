fdae3484fe6c46d546e182334d495710db75b9fc
emirmasi <emir12345masi@gmail.com>
2024-05-06 00:03:52 -0300

    fix

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/components/post/SettingImage.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/components/post/SettingImage.kt
@@ -1,6 +1,5 @@
 package ar.edu.unlam.mobile.scaffolding.ui.components.post
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -19,12 +18,8 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardColors
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -32,7 +27,6 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import ar.edu.unlam.mobile.scaffolding.R
@@ -125,24 +119,5 @@ fun SettingImage(
                 }
             }
         }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DialogImagePreview()  {
-    var showDialog by remember {
-        mutableStateOf(true)
-    }
-    Surface(
-        modifier = Modifier.background(MaterialTheme.colorScheme.background),
-    ) {
-        SettingImage(
-            item = null,
-            onDissmissButon = { showDialog = false },
-            onDeletePhoto = {},
-            onTakePhoto = {},
-            onUploadPhoto = {},
-        )
     }
 }
