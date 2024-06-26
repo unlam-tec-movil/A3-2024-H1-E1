ca07c8cd5ec8a0ebf45f98d98e1b514f54497423
emirmasi <emir12345masi@gmail.com>
2024-06-26 12:21:31 -0300

    fix ktlin

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/components/TextFieldOwn.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/components/TextFieldOwn.kt
@@ -1,10 +1,8 @@
 package ar.edu.unlam.mobile.scaffolding.ui.components
 
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -19,7 +17,6 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import ar.edu.unlam.mobile.scaffolding.ui.theme.BlueMarine
 
 @Composable
 fun TextFieldOwn(
