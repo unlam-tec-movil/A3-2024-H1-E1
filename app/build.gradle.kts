1bd24b2652ccd06951504a7aa3b5cfd90ceb554b
iamrochi <rmercado373@alumno.unlam.edu.ar>
2024-06-29 22:37:24 -0300

    test PublicationListViewModel

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -150,6 +150,9 @@ dependencies {
     testRuntimeOnly(libs.junit.jupiter.engine)
     debugImplementation(libs.androidx.test.core)
 
+    testImplementation("androidx.arch.core:core-testing:2.1.0")
+    testImplementation("io.mockk:mockk:1.12.0")
+
     // coil
     implementation("io.coil-kt:coil-compose:1.4.0")
 }
