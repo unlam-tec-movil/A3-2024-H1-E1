622fa06dec785a6e0e3df41a7acbdf6500eec8f3
Fabischaffts <124001856+Fabischaffts@users.noreply.github.com>
2024-06-30 23:52:22 -0300

    agrego test unitario
    se esta testeando unicamente la logica del viewModel lo que hace que es un test unitario

--- app/src/androidTest/java/ar/edu/unlam/mobile/scaffolding/FilterSettingTest.kt
+++ app/src/androidTest/java/ar/edu/unlam/mobile/scaffolding/FilterSettingTest.kt
@@ -8,20 +8,14 @@ import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import ar.edu.unlam.mobile.scaffolding.ui.screens.filterSettings.FilterSettingsScreen
 import ar.edu.unlam.mobile.scaffolding.ui.screens.filterSettings.FilterSettingsViewModel
-import dagger.hilt.android.testing.HiltAndroidRule
-import dagger.hilt.android.testing.HiltAndroidTest
 import junit.framework.TestCase.assertEquals
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-@HiltAndroidTest
 class FilterSettingsTest {
-    @get:Rule(order = 0)
-    var hiltRule = HiltAndroidRule(this)
-
-    @get:Rule(order = 1)
+    @get:Rule
     val composeTestRule = createComposeRule()
 
     @Test
@@ -39,8 +33,6 @@ class FilterSettingsTest {
         }
 
         composeTestRule.onNodeWithText("Distancia: 10 km").performClick()
-
-        // Simular aplicar los filtros
         composeTestRule.onNodeWithText("Aplicar Filtros").performClick()
 
         val filters = viewModel.getFilters()
